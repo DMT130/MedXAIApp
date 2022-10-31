4a4a4570848f7c53e3a7d9888f0f17a76bbc4f67
dmt130 <derciomichaque@gmail.com>
2022-10-31 15:37:33 +0200

    layout issues

--- frontx/src/App.js
+++ frontx/src/App.js
@@ -44,20 +44,20 @@ function App() {
 	return(
    		<div>
 			<div>
-				<div class="text-center">
+				<div className="text-center">
 					<input type="file"
 					onChange={changeHandler} />
 				</div>
 				<br/>
-				<div class="text-center">
+				<div className="text-center">
 					<button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
 					onClick={handleSubmission}>Predict</button>
 				</div>
 			</div>
 			<div className="grid grid-cols-1 md:grid-cols-6 gap-4 max-w-md md:max-w-6xl mx-auto">
-				<div class="...">
+				<div className="...">
 				<div>
-					<div class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+					<div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 						{isFilePicked ? (
 							<div>
 								<div>Filename: {selectedFile.name}</div>
@@ -71,12 +71,34 @@ function App() {
 								</div>
 							</div>
 						) : (
-							<div>Select a file to show details</div>
+							<div>
+								<p className="text-justify ...">Select a file to show details. It's recommended to upload DICOM files but png, jpg e jpeg files can also be used</p>
+								<br></br>
+								<p className="text-justify ...">Instructions: Select an image and press the Predict button, wait up to 15 seconds, the uploaded image will be posted bellow. Please note, the application does not save any image</p>
+								<br></br>
+								<p className="text-justify ...">This application can identiy the following deseases in torax XRAR images:</p>
+								<ol className="list-disc">
+									<li>Cardiomegaly</li>
+									<li>Aortic enlargement</li>
+									<li>Pleural thickening</li>
+									<li>ILD</li>
+									<li>Nodule/Mass</li>
+									<li>Pulmonary fibrosis</li>
+									<li>Lung Opacity</li>
+									<li>Atelectasis</li>
+									<li>Other lesion</li>
+									<li>Infiltration</li>
+									<li>Pleural effusion</li>
+									<li>Calcification</li>
+									<li>Consolidation</li>
+									<li>Pneumothorax</li>
+								</ol>
+							</div>
 						)}
 					</div>
 				</div>
 				</div>
-				<div class="col-span-5 ...">
+				<div className="col-span-5 ...">
 						{byteimg ? (
 							<img className="max-w-full h-auto" src={`data:image/jpeg;base64,${byteimg}`} alt="" />
 						) : (
